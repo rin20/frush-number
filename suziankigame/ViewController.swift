d6ba2b0f6fd7887bffa9f485d563b5fa937a6036
Kumagai rin <mitsublue@gmail.com>
2020-08-10 16:29:18 +0900

    Initial Commit

---
+++ suziankigame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  suziankigame
+//
+//  Created by Mitsu Kumagai on 2020/08/10.
+//  Copyright © 2020 kumagai rin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
